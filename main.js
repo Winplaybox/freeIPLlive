71c85ea868a3c5ff5a8816d71ba46ad1616099fc
Winplaybox <sagarsuthar530@gmail.com>
2020-09-28 23:04:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,136 +1,23 @@
-if(!navigator.getDisplayMedia && !navigator.mediaDevices.getDisplayMedia) {
-    var error = 'Your browser does NOT supports getDisplayMedia API.';
-    document.querySelector('h1').innerHTML = error;
+import { h, render } from "https://unpkg.com/preact@latest?module";
+// In case you need hooks uncomment this line
+// import {} from "https://unpkg.com/preact@latest/hooks/dist/hooks.module.js?module";
+import htm from "https://unpkg.com/htm?module";
 
-    document.querySelector('video').style.display = 'none';
-    document.getElementById('btn-start-recording').style.display = 'none';
-    document.getElementById('btn-stop-recording').style.display = 'none';
-    throw new Error(error);
-}
-const startElem = document.getElementById("start");
-const stopElem = document.getElementById("stop");
+// Initialize htm with Preact
+const html = htm.bind(h);
 
-function invokeGetDisplayMedia(success, error) {
-    var displaymediastreamconstraints = {
-        video: {
-            // displaySurface: 'monitor', // monitor, window, application, browser
-            logicalSurface: true,
-            cursor: 'always' // never, always, motion
-        }
-    };
+render(
+  html`
+    <${Plugin} />
+  `,
+  document.querySelector("#root")
+);
 
-    // above constraints are NOT supported YET
-    // that's why overridnig them
-    displaymediastreamconstraints = {
-        video: true
-    };
+function Plugin() {
+  const params = new URLSearchParams(window.location.search);
+  const username = params.get("username") || "World";
 
-    if(navigator.mediaDevices.getDisplayMedia) {
-        navigator.mediaDevices.getDisplayMedia(displaymediastreamconstraints).then(success).catch(error);
-    }
-    else {
-        navigator.getDisplayMedia(displaymediastreamconstraints).then(success).catch(error);
-    }
-}
-
-function captureScreen(callback) {
-    invokeGetDisplayMedia(function(screen) {
-        addStreamStopListener(screen, function() {
-            if(window.stopCallback) {
-                window.stopCallback();
-            }
-
-        });
-        callback(screen);
-    }, function(error) {
-        console.error(error);
-        alert('Unable to capture your screen. Please check console logs.\n' + error);
-    });
-}
-
-function captureCamera(cb) {
-    navigator.mediaDevices.getUserMedia({audio: true, video: true}).then(cb);
-}
-
-function keepStreamActive(stream) {
-    var video = document.createElement('video');
-    video.muted = true;
-    video.srcObject = stream;
-    video.style.display = 'none';
-    (document.body || document.documentElement).appendChild(video);
-}
-startElem.addEventListener("click", function(evt) {
-    captureScreen(function(screen) {
-        keepStreamActive(screen);
-        captureCamera(function(camera) {
-            keepStreamActive(camera);
-    
-            screen.width = window.screen.width;
-            screen.height = window.screen.height;
-            screen.fullcanvas = true;
-            
-            camera.width = 0;
-            camera.height = 0;
-            camera.top = screen.height - camera.height;
-            camera.left = screen.width - camera.width;
-            
-            var recorder = RecordRTC([screen, camera], {
-                type: 'video',
-                mimeType: 'video/webm',
-                previewStream: function(s) {
-                    document.querySelector('video').muted = false;
-                    document.querySelector('video').srcObject = s;
-                }
-            });
-    
-            recorder.startRecording();
-            setInterval(function(){
-            window.stopCallback = function() {
-                window.stopCallback = null;
-    
-                recorder.stopRecording(function() {
-                    var blob = recorder.getBlob();
-                    document.querySelector('video').srcObject = null;
-                    document.querySelector('video').src = URL.createObjectURL(blob);
-                    document.querySelector('video').muted = false;
-                    
-                    [screen, camera].forEach(function(stream) {
-                        stream.getTracks().forEach(function(track) {
-                            track.stop();
-                        });
-                    });
-                });
-            };
-            },1000);
-    
-            // window.timeout = setTimeout(window.stopCallback, 10 * 1000);
-        });
-    });
-  }, false);
-  
-stopElem.addEventListener("click", function(evt) {
-    if(window.stopCallback) {
-        window.stopCallback();
-    }
-  }, false);
-
-function addStreamStopListener(stream, callback) {
-    stream.addEventListener('ended', function() {
-        callback();
-        callback = function() {};
-    }, false);
-    stream.addEventListener('inactive', function() {
-        callback();
-        callback = function() {};
-    }, false);
-    stream.getTracks().forEach(function(track) {
-        track.addEventListener('ended', function() {
-            callback();
-            callback = function() {};
-        }, false);
-        track.addEventListener('inactive', function() {
-            callback();
-            callback = function() {};
-        }, false);
-    });
+  return html`
+    Hello ${username}
+  `;
 }
